1d5bf41eccfcaffa9b2ecd8a672a9298ae7b7f51
Manoj <94798999+manoj929@users.noreply.github.com>
2022-03-18 08:43:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  let extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch(extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break;
+    case '.css':
+      contentType = 'text/css'
+      break;
+    case "json":
+      contentType = "application/json";
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if(err) {
+      if(err.code == 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(content, 'utf-8');
+        })
+      } else {
+        res.writeHead(500);
+        res.end(`server error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.end(content, 'utf-8')
+    }
+  })
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+  console.log(`server is listening on ${PORT}`)
+})
